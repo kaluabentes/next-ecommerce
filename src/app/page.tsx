95f11f6752962082e05b6a5bc26ab0b7193fcca4
Kaluã Bentes <kaluanbentes@gmail.com>
2023-05-27 02:41:04 -0400

    feat: add home metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import { Metadata } from "next"
+
 import collectionItems from "@/config/collectionItems"
 import ProductsCarousel from "@/components/ProductsCarousel"
 
 import BannerCarousel from "./components/BannerCarousel"
 import CollectionsCarousel from "./components/CollectionsCarousel"
 import { getAllProducts } from "./api/products"
+
+export const metadata: Metadata = {
+  title: "Kalux Shop",
+  description:
+    "Bem-vindo à nossa loja online onde você encontrará uma ampla variedade de produtos cuidadosamente selecionados para atender às suas necessidades e desejos.",
+}
 
 export default async function Home() {
   const products = await getAllProducts([
