4643e5e41beab4794c23c526a10520fd3130c364
Kaluã Bentes <kaluanbentes@gmail.com>
2023-06-15 04:43:42 -0400

    refactor: add error handling

---
+++ src/app/checkout/hooks/useGetAddressByZipcode.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import cep from "cep-promise"
+import { CEP } from "cep-promise"
+import { FiAlertTriangle } from "react-icons/fi"
+import useToast from "@/app/design-system/Toast/context/useToast"
+
+export default function useGetAddressByZipcode() {
+  const toast = useToast()
+
+  const [zipcode, setZipcode] = useState("")
+  const [shippingInfo, setShippingInfo] = useState<CEP | undefined>()
+
+  useEffect(() => {
+    if (zipcode) {
+      const findAddress = async () => {
+        try {
+          await cep(zipcode.replace(/[^0-9]/g, "")).then((data: CEP) =>
+            setShippingInfo(data)
+          )
+        } catch (error: any) {
+          toast({
+            title: "Atenção",
+            description: "Verifique se o CEP está correto",
+            icon: <FiAlertTriangle />,
+          })
+        }
+      }
+
+      findAddress()
+    }
+  }, [zipcode])
+
+  return { setZipcode, shippingInfo }
+}
